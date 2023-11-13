60d3c5ab2f045d6434b873ee5a5a2232584a5f2a
dchae <daniel.j.chae@gmail.com>
2023-11-12 16:10:42 -0800

    Display playlists and top tracks; clear temp

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
@@ -31,11 +32,16 @@ def file_path(filename = nil, subfolder = 'temp')
   File.join(*[File.expand_path('..', __FILE__), subfolder, filename].compact)
 end
 
+def clear_temp
+  FileUtils.rm_rf(file_path)
+end
+
 def session_file_path(filename = nil)
   path = file_path(nil, 'temp/' + session[:session_id])
   FileUtils.mkdir_p(path) unless File.directory?(path)
   File.join([path, filename].compact)
 end
+
 
 def load_yaml(filepath)
   YAML.load_file(filepath) if File.file?(filepath)
@@ -64,50 +70,59 @@ def load_user_data(response)
   load_yaml(session_file_path('user_data.yml'))
 end
 
+def spotify_auth(clientId)
+  # Spotify API authentication
+  # clientId = My Spotify Dev App Client ID
+  redirectUri = 'http://localhost:4567/callback'
+  scope = 'user-read-private user-read-email user-top-read'
+  auth_url = 'https://accounts.spotify.com/authorize'
+  newuri =
+    URI::HTTP.build(
+      host: 'accounts.spotify.com',
+      path: '/authorize',
+      query:
+        URI.encode_www_form(
+          {
+            response_type: 'code',
+            client_id: clientId,
+            scope: scope,
+            code_challenge_method: 'S256',
+            code_challenge: @challenge[:code_challenge],
+            redirect_uri: redirectUri,
+          },
+        ),
+    )
+  redirect newuri
+end
+
+def spotify_api_call(endpoint = nil, access_token = session[:access_token])
+  HTTParty.get(
+    "https://api.spotify.com/v1#{endpoint}",
+    { headers: { Authorization: 'Bearer ' + access_token } },
+  )
+end
+
+clear_temp
+get '/test' do
+  response = spotify_api_call('/me')
+end
+
 get '/' do
   redirect '/play'
 end
 
 get '/play' do
-  # IF NOT AUTHENTICATED, DO:
   # need to implement:
   # if access token is expired, get new token with session[:refresh_token]
-  unless session[:access_token]
-    # Spotify API authentication
-    clientId = 'a6c331f9a46f4198904b69b4dea82f74' # My Spotify Dev App Client ID
-    redirectUri = 'http://localhost:4567/callback'
-    scope = 'user-read-private user-read-email'
-    auth_url = 'https://accounts.spotify.com/authorize'
-    newuri =
-      URI::HTTP.build(
-        host: 'accounts.spotify.com',
-        path: '/authorize',
-        query:
-          URI.encode_www_form(
-            {
-              response_type: 'code',
-              client_id: clientId,
-              scope: scope,
-              code_challenge_method: 'S256',
-              code_challenge: @challenge[:code_challenge],
-              redirect_uri: redirectUri,
-            },
-          ),
-      )
-    redirect newuri
-  end
-  response =
-    HTTParty.get(
-      'https://api.spotify.com/v1/me',
-      { headers: { Authorization: 'Bearer ' + session[:access_token] } },
-    )
-  @display_name = response['display_name']
-  @profile_url = response['external_urls']['spotify']
-  @id = response["id"]
-  @profile_image = response["images"][1]["url"]
-  @follower_count = response["followers"]["total"]
-  @country = response["country"]
-  @email = response["email"]
+  spotify_auth('a6c331f9a46f4198904b69b4dea82f74') unless session[:access_token]
+  response = spotify_api_call('/me')
+  @user_data = response
+  response = spotify_api_call('/me/playlists')
+  @playlists = response['items']
+
+  response = spotify_api_call('/me/top/tracks')
+  @top_tracks = response['items']
+
   erb :spotify_profile_data
 end
 
